25252aaa07f4abd9f940bd4fa515b18a88a366a2
VS-LIU <vctl95@gmail.com>
2024-02-14 18:53:45 -0800

    Revise class `Elf` to implement interfaces `Minion` and `Companion`

--- app/src/main/java/com/example/lab5victorliu/Elf.kt
+++ app/src/main/java/com/example/lab5victorliu/Elf.kt
@@ -5,7 +5,7 @@ package com.example.lab5victorliu
     Victor Liu - A00971668 - set C
  */
 
-class Elf: Minion() {
+class Elf: Minion, Companion {
     override val race: String
         get() = "Elf"
     override val baseHealth: Int
@@ -16,5 +16,25 @@ class Elf: Minion() {
         get() = 3
     override val catchphrase: String
         get() = "My arrows never miss!"
+
+    override fun huntReward(value: Int): String {
+        return when (value) {
+            in 11..20 -> {
+                "Fish"
+            }
+            in 21..30 -> {
+                "Forest Bear"
+            }
+            in 31..40 -> {
+                "Orc"
+            }
+            in 41..60 -> {
+                "Troll"
+            }
+            else -> {
+                "Nothing"
+            }
+        }
+    }
 
 }
